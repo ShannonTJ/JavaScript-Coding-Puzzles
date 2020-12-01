ad23bd8274529ab44c5afaf99dcfebcfd4da0b8a
Shannon TJ <shannontj@shaw.ca>
2020-12-01 14:23:18 -0700

    components setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { configure } from "@testing-library/react";
 import React from "react";
+import { Grid } from "@material-ui/core";
+import SearchBar from "./components/SearchBar";
+import { VideoList } from "./components/VideoList";
+import VideoDetail from "./components/VideoDetail";
+import youtube from "./api/youtube";
 
 class App extends React.Component {
   render() {
-    return <h1>Video Player App</h1>;
+    return (
+      <Grid justify="center" container spacing={16}>
+        <Grid item xs={11}>
+          <Grid container spacing={16}>
+            <Grid item xs={12}>
+              <SearchBar />
+              <Grid item xs={8}>
+                <VideoDetail />
+              </Grid>
+              <Grid item xs={8}>
+                {/* //VIDEO LIST */}
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "",
-  params: {},
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: "AIzaSyBMKceVdkO6zXuPxkUwHphdrg54hnkT8pI",
+  },
 });
